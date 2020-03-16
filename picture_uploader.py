e80035a0e2a4672130378890c7404da94313a115
AndreaTroianiello94 <andrea.troianiello@leroymerlin.it>
2020-03-16 09:09:55 +0100

    Fix response error

--- picture_uploader.py
+++ picture_uploader.py
@@ -16,20 +16,24 @@ def upload_image(request):
     response = {}
     if request.method != 'POST':
         response['message']='Wrong method'
-    if 'file' not in request.files:
+    elif 'file' not in request.files:
         response['success'] = False
         response['message'] = 'No file part'
-    file = request.files['file']
-    if file.filename == '':
-        response['success']=False
-        response['message']='No selected file'
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        client = storage.Client()
-        #get_bucket('bucket-id') <- https://console.cloud.google.com/storage/browser/[bucket-id]/
-        bucket = client.get_bucket('lmit-flask-example')
-        blob=bucket.blob('/' + filename) #Path and File name that will be saved.
-        blob.upload_from_file(file)
-        response['success']=True
-        response['message']='Upload done'
+    else: 
+        file = request.files['file']
+        if file.filename == '':
+            response['success']=False
+            response['message']='No selected file'
+        elif file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            client = storage.Client()
+            #get_bucket('bucket-id') <- https://console.cloud.google.com/storage/browser/[bucket-id]/
+            bucket = client.get_bucket('lmit-flask-example')
+            blob=bucket.blob('/' + filename) #Path and File name that will be saved.
+            blob.upload_from_file(file)
+            response['success']=True
+            response['message']='Upload done'
+        else: 
+            response['success'] = False
+            response['message'] = 'Invalid file'
     return jsonify(response)
